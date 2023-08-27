1800f69769b4e087a9182cab45d4d5a3c24fd931
igorLAO <igooli670@gmai.com>
2023-08-27 13:21:21 -0300

    links

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,8 +3,20 @@ import googlePlay from '../../assets/images/google-play.png'
 import microsoft from '../../assets/images/microsoft.png'
 
 import './index.scss';
+import { useState, useRef } from 'react';
+
 
 function App() {
+  const [hidePlaceHolder, setHidePlaceHolder] = useState(false)
+  const EmailRef = useRef(null)
+  const senhaRef = useRef(null)
+
+
+  function blurInput() {
+    setHidePlaceHolder(true)
+  }
+
+
   return (
     <div className="App">
       <div className='loginScreen'>
@@ -13,29 +25,29 @@ function App() {
 
         </div>
         <div className='inputs'>
-          
-          <div>
-            <span>Telefone, nome de usuario ou email</span>
-            <input type='text' placeholder='' />
+
+          <div onClick={() => EmailRef.current.focus(EmailRef)}>
+
+            <input ref={EmailRef}  type='text' placeholder='Telefone, nome de usuario ou email' />
+
           </div>
 
-          <div>
-            <span> senha</span>
-            <input type='password' />
+          <div onClick={() => senhaRef.current.focus(senhaRef)}>
+            <input ref={senhaRef} type='password' placeholder='Senha' />
           </div>
           <button>Entrar</button>
         </div>
         <a className='RecPwd' href=''>Esqueceu a senha?</a>
       </div>
       <div className='NewAccount'>
-        <p>Não tem uma conta? <span>Cadastre-se</span></p>
+        <p>Não tem uma conta? <a href=''>Cadastre-se</a></p>
       </div>
       <div className='getApp'>
-          <p>Obtenha o aplicativo</p>
-          <div className='sites'>
-            <img src={googlePlay} className='googlePlay' />
-            <img src={microsoft} className='microsoft'/>
-          </div>
+        <p>Obtenha o aplicativo</p>
+        <div className='sites'>
+          <img src={googlePlay} className='googlePlay' />
+          <img src={microsoft} className='microsoft' />
+        </div>
       </div>
 
       <footer>
@@ -57,9 +69,9 @@ function App() {
           <a href='' >Principais</a>
         </div>
         <div>
-          <a href=''>Português (Brasil)  </a> 
+          <a href=''>Português (Brasil)  </a>
           <a href=''>
-          &copy; 2023 Instagram from Meta
+            &copy; 2023 Instagram from Meta
           </a>
         </div>
       </footer>
